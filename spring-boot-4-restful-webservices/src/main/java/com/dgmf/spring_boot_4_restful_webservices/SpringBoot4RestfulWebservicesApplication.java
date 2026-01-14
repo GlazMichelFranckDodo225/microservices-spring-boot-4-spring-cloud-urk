761048b2c94b43a02c86c828f97711164e4b3caf
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-01-14 07:58:03 +0100

    Project Initialization

---
+++ spring-boot-4-restful-webservices/src/main/java/com/dgmf/spring_boot_4_restful_webservices/SpringBoot4RestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.spring_boot_4_restful_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4RestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-4-restful-webservices/src/test/java/com/dgmf/spring_boot_4_restful_webservices/SpringBoot4RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.spring_boot_4_restful_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
