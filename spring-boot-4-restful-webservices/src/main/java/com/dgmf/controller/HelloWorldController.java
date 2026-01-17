b43e6d7a2d12f9bba00baf036fe3bda08dcc7be0
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-01-17 08:09:27 +0100

    Creating a Hello World REST API with Spring Boot | @GetMapping() Annotation

--- spring-boot-4-restful-webservices/src/main/java/com/dgmf/controller/HelloWorldController.java
+++ spring-boot-4-restful-webservices/src/main/java/com/dgmf/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.dgmf.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
+    // @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
     }
